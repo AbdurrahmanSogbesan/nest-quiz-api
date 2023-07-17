e7fb3722d00f165a9117ab847062294aab862e5c
Abdurrahman <abdurrahman0803@gmail.com>
2023-07-17 21:29:13 +0100

    chore: code cleanup

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.model';
 import { UserController } from './user.controller';
 
+const UserFeature = MongooseModule.forFeature([
+  { name: User.name, schema: UserSchema },
+]);
+
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  imports: [UserFeature],
   controllers: [UserController],
-  exports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  exports: [UserFeature],
 })
 export class UserModule {}
